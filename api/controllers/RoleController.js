bd4282808d27e1d150ac0d66f7b1fbcb9e01460d
camikazegreen <camikazegreen@gmail.com>
2015-07-10 18:30:29 -0700

    just log the whole body

--- api/controllers/RoleController.js
+++ api/controllers/RoleController.js
@@ -9,6 +9,7 @@ _.merge(exports, {
   // Extend with custom logic here by adding additional fields, methods, etc.
 
   update: function(req,res){
+    sails.log(req.body);
     sails.log(req.body.username);
     sails.log(req.body.add);
     sails.log(req.body.remove);
