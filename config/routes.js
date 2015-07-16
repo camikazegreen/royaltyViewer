0d5f7fa1f83c77239f366ec527d9d0e7354a63ae
camikazegreen <camikazegreen@gmail.com>
2015-07-15 20:58:06 -0700

    adding the new client view/api

--- config/routes.js
+++ config/routes.js
@@ -35,6 +35,9 @@ module.exports.routes = {
   '/': {
     view: 'homepage'
   },
+  '/client/new':{
+    view: 'client/new'
+  },
   
 
   /***************************************************************************
