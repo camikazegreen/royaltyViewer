77214ec4654d2f6b8b138e9135c48566be7b6cb4
camikazegreen <camikazegreen@gmail.com>
2015-08-03 19:18:46 -0700

    adding in the full address field

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -16,6 +16,9 @@ module.exports = {
 		Client.find().populate('company')
 		.exec(function(err,results){
 			if(err){console.log(err)};
+			for (var i = results.length - 1; i >= 0; i--) {
+				results[i].address = "<div><div>"+results[i].address1+"</div><div>"+results[i].address2+"</div><span>"+results[i].city+",</span><span>"+results[i].state+"</span><span>"+results[i].zip+"<span></div"
+			};
 			console.log("No error ",results);
 			return res.view('client/manage',{clients:results})
 		})
