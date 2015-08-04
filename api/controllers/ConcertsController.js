178c01e8d418243862b09a961b06e5dd8129dfb0
camikazegreen <camikazegreen@gmail.com>
2015-08-04 11:54:20 -0700

    narrow it down.

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -11,81 +11,81 @@ module.exports = {
 		Client.find.populate('band')
 		.exec(function(e,r){
 			if(e){console.log(e)};
-function getConcerts(city,artist){
-	var options = {
-		host: 'api.songkick.com',
-		path: '/api/3.0/events.json?apikey='+apikey+'&location='+city+'&artist_name='+artist
-	};
-	console.log('getting concerts for '+options.path)
+// function getConcerts(city,artist){
+// 	var options = {
+// 		host: 'api.songkick.com',
+// 		path: '/api/3.0/events.json?apikey='+apikey+'&location='+city+'&artist_name='+artist
+// 	};
+// 	console.log('getting concerts for '+options.path)
 
-	var callback = function(response){
-		var str = '';
+// 	var callback = function(response){
+// 		var str = '';
 
-		response.on('data',function(chunk){
-			str += chunk;
-		});
+// 		response.on('data',function(chunk){
+// 			str += chunk;
+// 		});
 
-		response.on('end', function(){
-			var json = JSON.parse(str);
-			if(json.resultsPage.totalEntries>0){
-				var conString = JSON.stringify(json.resultsPage.results.event);
-			if(city==phoenix){
-			// concerts.phoenix.push(details);
-			Concerts.create({location:'phoenix'},{string:conString}, function(err, concert){
-      if (err){
-        console.log(err);
-      }
-      else{
-        console.log(artist+" in "+city)
-      }
-    });
-		}else if(city==newYork){
-			concerts.newYork.push(details);
-			Concerts.create({location:'newYork',string:conString}, function(err, concert){
-      if (err){
-        console.log(err);
-      }
-      else{
-        console.log(artist+" in "+city)
-      }
-    });
-		}else if(city==losAngeles){
-			// concerts.losAngeles.push(details);
-			Concerts.create({location:'losAngeles',string:conString}, function(err, concert){
-      if (err){
-        console.log(err);
-      }
-      else{
-        console.log(artist+" in "+city)
-      }
-    });
-		}else if(city==nashville){
-			// concerts.nashville.push(details);
-			Concerts.create({location:'nashville',string:conString}, function(err, concert){
-      if (err){
-        console.log(err);
-      }
-      else{
-        console.log(artist+" in "+city)
-      }
-    });
-		}else if(city==tucson){
-			// concerts.tucson.push(details);
-			Concerts.create({location:'tucson',string:conString}, function(err, concert){
-      if (err){
-        console.log(err);
-      }
-      else{
-        console.log(artist+" in "+city)
-      }// else
-    });//Concerts.create
-		}// if tucson
-			}//if totalEntries > 0
+// 		response.on('end', function(){
+// 			var json = JSON.parse(str);
+// 			if(json.resultsPage.totalEntries>0){
+// 				var conString = JSON.stringify(json.resultsPage.results.event);
+// 			if(city==phoenix){
+// 			// concerts.phoenix.push(details);
+// 			Concerts.create({location:'phoenix'},{string:conString}, function(err, concert){
+//       if (err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log(artist+" in "+city)
+//       }
+//     });
+// 		}else if(city==newYork){
+// 			concerts.newYork.push(details);
+// 			Concerts.create({location:'newYork',string:conString}, function(err, concert){
+//       if (err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log(artist+" in "+city)
+//       }
+//     });
+// 		}else if(city==losAngeles){
+// 			// concerts.losAngeles.push(details);
+// 			Concerts.create({location:'losAngeles',string:conString}, function(err, concert){
+//       if (err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log(artist+" in "+city)
+//       }
+//     });
+// 		}else if(city==nashville){
+// 			// concerts.nashville.push(details);
+// 			Concerts.create({location:'nashville',string:conString}, function(err, concert){
+//       if (err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log(artist+" in "+city)
+//       }
+//     });
+// 		}else if(city==tucson){
+// 			// concerts.tucson.push(details);
+// 			Concerts.create({location:'tucson',string:conString}, function(err, concert){
+//       if (err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log(artist+" in "+city)
+//       }// else
+//     });//Concerts.create
+// 		}// if tucson
+// 			}//if totalEntries > 0
 
-		});
-	}
-	http.request(options, callback).end();
-}//getConcerts
+// 		});
+// 	}
+// 	http.request(options, callback).end();
+// }//getConcerts
 		var concerts = {};
 		concerts.newYork = [];
 		concerts.losAngeles = [];
@@ -99,7 +99,8 @@ function getConcerts(city,artist){
 		var tucson = "sk:10046";
 		var phoenix = "sk:23068";
 		for (var i = r.length - 1; i >= 0; i--) {
-			getConcerts(newYork,r[i].performsas);
+			concerts.push(r[i]);
+			// getConcerts(newYork,r[i].performsas);
 		};
 		return res.view('concerts',{concerts:concerts})
 		})
