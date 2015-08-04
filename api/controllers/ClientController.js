835e296cf26cc5021ee6944e1504d5991e7389cd
camikazegreen <camikazegreen@gmail.com>
2015-08-03 19:26:32 -0700

    fixed formatting in the address

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -17,7 +17,9 @@ module.exports = {
 		.exec(function(err,results){
 			if(err){console.log(err)};
 			for (var i = results.length - 1; i >= 0; i--) {
-				results[i].address = "<div><div>"+results[i].address1+"</div><div>"+results[i].address2+"</div><span>"+results[i].city+",</span><span>"+results[i].state+"</span><span>"+results[i].zip+"<span></div"
+				var address = results[i].address1;
+				if(results[i].address2){address += "<br>"+results[i].address2};
+				results[i].address = "<div class='smallAddress'><div>"+address+"</div><span>"+results[i].city+", </span><span>"+results[i].state+" </span><span>"+results[i].zip+"<span></div"
 			};
 			console.log("No error in",results);
 			return res.view('client/manage',{clients:results})
