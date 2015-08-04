73046f514a0b738f8d3832609ad1c628767494f3
camikazegreen <camikazegreen@gmail.com>
2015-08-04 15:46:59 +0000

    ignored

---
+++ api/controllers/apikeys.js
@@ -0,0 +1,15 @@
+exports.s3keys=[
+{
+  key: 'AKIAI5PFOJ373VKWCYMA',
+  secret: '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'
+}
+]
+exports.echonestkeys=[
+{
+  apikey: 'ESBJ96UO5MMIFCFGJ',
+  conskey: 'e23b2d3a3c020566cb93f568e78cd69d',
+  sharedSecret: 'YxrP6CGtQJCOG0om6zFUZQ'
+}
+]
+exports.songkickkeys=[
+{apikey: 'JG4EYpMiQzrHumjA'}]
