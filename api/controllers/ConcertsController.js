5bd799698d1d94b7057be265208247aa20ec1217
camikazegreen <camikazegreen@gmail.com>
2015-08-04 15:33:21 +0000

    created the api

---
+++ api/controllers/ConcertsController.js
@@ -0,0 +1,11 @@
+/**
+ * ConcertsController
+ *
+ * @description :: Server-side logic for managing concerts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
