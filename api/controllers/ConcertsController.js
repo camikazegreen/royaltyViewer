ac72026fd2f32686c5175940922f7f3c582fa51c
camikazegreen <camikazegreen@gmail.com>
2015-08-28 19:30:20 -0700

    display the view

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -8,11 +8,16 @@
 
 module.exports = {
 	view: function(req,res){
-		Concerts.find()
+		var concerts = {};
+		concerts.newyork = [];
+		Concerts.find({ where: { location: 'New York' }})
 		.exec(function(e,r){
 			if(e){console.log(e)};
 			console.log(r);
-		 // return res.view('concerts',{concerts:concerts})
+			for (var i = r.length - 1; i >= 0; i--) {
+				concerts.newyork.push(r[i]);
+			};
+		 return res.view('concerts',{concerts:concerts})
 		})
 	}
 };
