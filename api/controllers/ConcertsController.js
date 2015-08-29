3c6032a44134f307ce70aedaeb9bc4d08a41149a
camikazegreen <camikazegreen@gmail.com>
2015-08-28 20:34:49 -0700

    Getting it back to working

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -21,38 +21,38 @@ module.exports = {
 				concerts.newyork.push(r[i]);
 			};
 		})
-		Concerts.find({ where: { location: 'Los Angeles' },date:{'>':new Date()}})
-		.exec(function(e,r){
-			if(e){console.log(e)};
-			console.log(r);
-			for (var i = r.length - 1; i >= 0; i--) {
-				concerts.losAngeles.push(r[i]);
-			};
-		})
-		Concerts.find({ where: { location: 'Nashville' },date:{'>':new Date()}})
-		.exec(function(e,r){
-			if(e){console.log(e)};
-			console.log(r);
-			for (var i = r.length - 1; i >= 0; i--) {
-				concerts.nashville.push(r[i]);
-			};
-		})
-		Concerts.find({ where: { location: 'Tucson' },date:{'>':new Date()}})
-		.exec(function(e,r){
-			if(e){console.log(e)};
-			console.log(r);
-			for (var i = r.length - 1; i >= 0; i--) {
-				concerts.arizona.push(r[i]);
-			};
-		})
-		Concerts.find({ where: { location: 'Phoenix' },date:{'>':new Date()}})
-		.exec(function(e,r){
-			if(e){console.log(e)};
-			console.log(r);
-			for (var i = r.length - 1; i >= 0; i--) {
-				concerts.arizona.push(r[i]);
-			};
-		})
+		// Concerts.find({ where: { location: 'Los Angeles' },date:{'>':new Date()}})
+		// .exec(function(e,r){
+		// 	if(e){console.log(e)};
+		// 	console.log(r);
+		// 	for (var i = r.length - 1; i >= 0; i--) {
+		// 		concerts.losAngeles.push(r[i]);
+		// 	};
+		// })
+		// Concerts.find({ where: { location: 'Nashville' },date:{'>':new Date()}})
+		// .exec(function(e,r){
+		// 	if(e){console.log(e)};
+		// 	console.log(r);
+		// 	for (var i = r.length - 1; i >= 0; i--) {
+		// 		concerts.nashville.push(r[i]);
+		// 	};
+		// })
+		// Concerts.find({ where: { location: 'Tucson' },date:{'>':new Date()}})
+		// .exec(function(e,r){
+		// 	if(e){console.log(e)};
+		// 	console.log(r);
+		// 	for (var i = r.length - 1; i >= 0; i--) {
+		// 		concerts.arizona.push(r[i]);
+		// 	};
+		// })
+		// Concerts.find({ where: { location: 'Phoenix' },date:{'>':new Date()}})
+		// .exec(function(e,r){
+		// 	if(e){console.log(e)};
+		// 	console.log(r);
+		// 	for (var i = r.length - 1; i >= 0; i--) {
+		// 		concerts.arizona.push(r[i]);
+		// 	};
+		// })
 
 
 
