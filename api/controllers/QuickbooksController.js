d94a3d41be0b1c9b76db3a5bcb266dc0a33ee5b1
camikazegreen <camikazegreen@gmail.com>
2015-08-31 23:45:38 +0000

    adding qb api

---
+++ api/controllers/QuickbooksController.js
@@ -0,0 +1,11 @@
+/**
+ * QuickbooksController
+ *
+ * @description :: Server-side logic for managing quickbooks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
