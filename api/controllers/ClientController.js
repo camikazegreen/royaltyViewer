0b8e259cff764ed3d86ab206933a20a27c67c1e1
camikazegreen <camikazegreen@gmail.com>
2015-08-31 14:08:38 -0700

    address update, style change, safe migrate

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -18,9 +18,11 @@ module.exports = {
 			if(err){console.log(err)};
 			for (var i = results.length - 1; i >= 0; i--) {
 				results[i].company = results[i].company.name;
-				var address = results[i].address1;
-				if(results[i].address2){address += "<br>"+results[i].address2};
-				results[i].address = "<div class='smallAddress'><div>"+address+"</div><span>"+results[i].city+", </span><span>"+results[i].state+" </span><span>"+results[i].zip+"<span></div"
+				if(results[i].address1){
+					var address = results[i].address1;
+					if(results[i].address2){address += "<br>"+results[i].address2};
+					results[i].address = "<div class='smallAddress'><div>"+address+"</div><span>"+results[i].city+", </span><span>"+results[i].state+" </span><span>"+results[i].zip+"<span></div"
+				};
 			};
 			console.log("No error in",results);
 			return res.view('client/manage',{clients:results})
