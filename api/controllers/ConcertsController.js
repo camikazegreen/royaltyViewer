0cb0d365ec88151b87f7b180c2fdde1ec80060d6
camikazegreen <camikazegreen@gmail.com>
2015-08-31 14:51:08 -0700

    Add all cities to concert view

--- api/controllers/ConcertsController.js
+++ api/controllers/ConcertsController.js
@@ -8,6 +8,7 @@
 
 module.exports = {
 	view: function(req,res){
+		var counter = 0;
 		var concerts = {};
 		concerts.newyork = [];
 		concerts.losAngeles = [];
@@ -19,44 +20,54 @@ module.exports = {
 			for (var i = r.length - 1; i >= 0; i--) {
 				concerts.newyork.push(r[i]);
 			};
-			console.log(concerts);
-
-		 return res.view('concerts',{concerts:concerts})
+			counter++;console.log("NY="+counter);
+			checkDone();
 		})
-		// Concerts.find({ where: { location: 'Los Angeles' },date:{'>':new Date()}})
-		// .exec(function(e,r){
-		// 	if(e){console.log(e)};
-		// 	console.log(r);
-		// 	for (var i = r.length - 1; i >= 0; i--) {
-		// 		concerts.losAngeles.push(r[i]);
-		// 	};
-		// })
-		// Concerts.find({ where: { location: 'Nashville' },date:{'>':new Date()}})
-		// .exec(function(e,r){
-		// 	if(e){console.log(e)};
-		// 	console.log(r);
-		// 	for (var i = r.length - 1; i >= 0; i--) {
-		// 		concerts.nashville.push(r[i]);
-		// 	};
-		// })
-		// Concerts.find({ where: { location: 'Tucson' },date:{'>':new Date()}})
-		// .exec(function(e,r){
-		// 	if(e){console.log(e)};
-		// 	console.log(r);
-		// 	for (var i = r.length - 1; i >= 0; i--) {
-		// 		concerts.arizona.push(r[i]);
-		// 	};
-		// })
-		// Concerts.find({ where: { location: 'Phoenix' },date:{'>':new Date()}})
-		// .exec(function(e,r){
-		// 	if(e){console.log(e)};
-		// 	console.log(r);
-		// 	for (var i = r.length - 1; i >= 0; i--) {
-		// 		concerts.arizona.push(r[i]);
-		// 	};
-		// })
+		Concerts.find({ where: { location: 'Los Angeles' },date:{'>':new Date()}, sort: 'date DESC'})
+		.exec(function(e,r){
+			if(e){console.log(e)};
+			for (var i = r.length - 1; i >= 0; i--) {
+				concerts.losAngeles.push(r[i]);
+			};
+			counter++;console.log("LA="+counter);
+			checkDone();
+		})
+		Concerts.find({ where: { location: 'Nashville' },date:{'>':new Date()}, sort: 'date DESC'})
+		.exec(function(e,r){
+			if(e){console.log(e)};
+			console.log(r);
+			for (var i = r.length - 1; i >= 0; i--) {
+				concerts.nashville.push(r[i]);
+			};
+			counter++;console.log("NV="+counter);
+			checkDone();
+		})
+		Concerts.find({ where: { location: 'Tucson' },date:{'>':new Date()}, sort: 'date DESC'})
+		.exec(function(e,r){
+			if(e){console.log(e)};
+			console.log(r);
+			for (var i = r.length - 1; i >= 0; i--) {
+				concerts.arizona.push(r[i]);
+			};
+			counter++;
+			checkDone();
+		})
+		Concerts.find({ where: { location: 'Phoenix' },date:{'>':new Date()}, sort: 'date DESC'})
+		.exec(function(e,r){
+			if(e){console.log(e)};
+			console.log(r);
+			for (var i = r.length - 1; i >= 0; i--) {
+				concerts.arizona.push(r[i]);
+			};
+			counter++;console.log("AZ="+counter);
+			checkDone();
+		})
 
-
+		function checkDone(){
+			if(counter==4){	
+		 		return res.view('concerts',{concerts:concerts})
+			}
+		}
 
 
 	}
