3bfa75b77af99ecc2a5d56c9704587768d4f8360
camikazegreen <camikazegreen@gmail.com>
2015-10-06 08:33:55 -0700

    show all the data

--- api/controllers/QuickbooksController.js
+++ api/controllers/QuickbooksController.js
@@ -105,7 +105,7 @@ module.exports = {
 		})
 		qbo.findVendors(" where GivenName = 'Alison'",function(err,vendors){
 			console.log(vendors);
-			return res.view('quickbooks/manage',{vendors:vendors.Vendor})
+			return res.view('quickbooks/manage',{vendors:vendors})
 		})
 
 		// res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
