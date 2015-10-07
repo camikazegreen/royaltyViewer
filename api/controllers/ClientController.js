c72cd8f7a7d0a224a8c434532ea3ba5cf6e79b24
camikazegreen <camikazegreen@gmail.com>
2015-10-06 18:12:07 -0700

    keep it simple

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -31,33 +31,18 @@ module.exports = {
 	import: function(req,res){
 		var fs = require('fs');
 		var parse = require('csv/node_modules/csv-parse');
-		var parser = parse(function(err,data){
-				console.log(data);
-			})
 		var output = [];
-		parser.on('readable',function(){
-			console.log('readable');
-			console.log(parser.read());
-			while(record = parser.read()){
-				output.push(record);
+
+		req.file('csvFile').upload({maxBytes:10000000},function whenDone(err, uploadedFiles){
+			if(err){return res.negotiate(err);
 			}
-		});
-		parser.on('error', function(err){
-			console.log(err.message);
-		});
-		parser.on('finish',function(){
-			console.log('finished');
-		});
-		parser.write("something");
-		parser.end();
-		// req.file('csvFile').upload({maxBytes:10000000},function whenDone(err, uploadedFiles){
-		// 	if(err){return res.negotiate(err);
-		// 	}
-		// 	if (uploadedFiles.length === 0){
-		// 		return res.badRequest('No file was uploaded');
-		// 	}
-		// 	fs.createReadStream(uploadedFiles).pipe(parser);
-		// })
+			if (uploadedFiles.length === 0){
+				return res.badRequest('No file was uploaded');
+			}
+			parse(uploadedFiles,{},funtion(err,output){
+				console.log(output);
+			})
+		})
 	}
 };
 
