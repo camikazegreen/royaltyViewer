85d8491e0cfb1e681eb991a6a089c166b005cc29
camikazegreen <camikazegreen@gmail.com>
2015-10-08 09:41:47 -0700

    set it to null

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -71,7 +71,7 @@ module.exports = {
 				startdate:data[i]['Contract Date'],
 				enddate:data[i]['Expiry Date'],
 				mmcode:data[i].Code,
-				qbcode:parseInt(""),
+				qbcode:null,
 				held:data[i].Held,
 				Rrate:data[i]['[R]'],
 				SRrate:data[i]['[SR]']
